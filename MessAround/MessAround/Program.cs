44042545df51d95e084727ef5fff2e79052cab57
Rashan Whittaker <Rashan141@users.noreply.github.com>
2018-01-21 11:43:32 -0600

    Fresh work

---
+++ MessAround/MessAround/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MessAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 1, 2, 3 });
+        }
+    }
+}
